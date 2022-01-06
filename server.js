9baef0173a451832ec554b81e5694631a5a3c60e
Ofek Asido <asido.ofek@gmail.com>
2022-01-07 00:38:15 +0200

    Implement passport-local-mongoose & create test

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const app = express();
 const PORT = 4080;
 require('dotenv').config();
@@ -7,6 +8,15 @@ const mongoose = require('mongoose');
 const User = require('./models/user');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/shift-scheduler');
 
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get('/', async (req, res) => {
+  const user = new User({ username: 'user' });
+  await user.setPassword('password');
+  await user.save();
+  res.send('done');
 });
 
 app.listen(PORT, console.log(`Server is running on http://localhost:${PORT}`));
