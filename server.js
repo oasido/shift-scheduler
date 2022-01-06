27cb6f5418db581c7518e6d534b1d1f7dd632492
Ofek Asido <asido.ofek@gmail.com>
2022-01-07 00:37:30 +0200

    Connect to DB and load the User model

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const PORT = 4080;
 require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send('test');
+const mongoose = require('mongoose');
+const User = require('./models/user');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/shift-scheduler');
+
 });
 
 app.listen(PORT, console.log(`Server is running on http://localhost:${PORT}`));
