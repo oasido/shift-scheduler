7bd9450facf54cc33ee356c4e053b7587eb56c46
Ofek Asido <asido.ofek@gmail.com>
2022-01-07 00:39:27 +0200

    Disable the selection of previous days

--- src/components/EmployeeAvailability/DateInput.tsx
+++ src/components/EmployeeAvailability/DateInput.tsx
@@ -8,10 +8,12 @@ import he from 'date-fns/locale/he';
 export default function DateInput({ className }) {
   const [selected, setSelected] = useState<Date>();
 
+  const today = new Date();
+
   let footer = 'יש לבחור תאריך';
   if (selected) {
     footer = `${format(selected, 'EEEE, d LLLL yyyy', { locale: he })}.`;
     console.log(selected);
   }
-  return <DayPicker className={className} dir="rtl" locale={he} mode="single" selected={selected} onSelect={setSelected} footer={footer} />;
+  return <DayPicker className={className} dir="rtl" locale={he} mode="single" selected={selected} onSelect={setSelected} footer={footer} disabled={{ before: today }} />;
 }
