fe7e12a179d30701361cff00f844a3f50dad86b3
Ofek Asido <asido.ofek@gmail.com>
2022-01-07 23:58:16 +0200

    Pass down state to date component

--- src/components/EmployeeAvailability/AvailabilityPage.jsx
+++ src/components/EmployeeAvailability/AvailabilityPage.jsx
@@ -3,6 +3,22 @@ import DateInput from './DateInput.tsx';
 import Table from './Table';
 
 const AvailabilityPage = () => {
+  const [selected, setSelected] = useState(null);
+
+  const today = new Date();
+
+  let footer = 'יש לבחור תאריך';
+  if (selected) {
+    footer = `${format(selected, 'EEEE, d LLLL yyyy', { locale: he })}.`;
+    console.log(selected);
+  }
+
+  let props = {
+    selected,
+    setSelected,
+    footer,
+    today,
+  };
   return (
     <>
       <DateInput className="scale-[1.15] m-10 md:scale-[1.35] md:m-20 " />
--- src/components/EmployeeAvailability/DateInput.tsx
+++ src/components/EmployeeAvailability/DateInput.tsx
@@ -5,15 +5,17 @@ import { format } from 'date-fns';
 
 import he from 'date-fns/locale/he';
 
-export default function DateInput({ className }) {
-  const [selected, setSelected] = useState<Date>();
-
-  const today = new Date();
-
-  let footer = 'יש לבחור תאריך';
-  if (selected) {
-    footer = `${format(selected, 'EEEE, d LLLL yyyy', { locale: he })}.`;
-    console.log(selected);
-  }
-  return <DayPicker className={className} dir="rtl" locale={he} mode="single" selected={selected} onSelect={setSelected} footer={footer} disabled={{ before: today }} />;
+export default function DateInput(props) {
+  return (
+    <DayPicker
+      className="scale-[1.15] m-10 md:scale-[1.35] md:m-20 "
+      dir="rtl"
+      locale={he}
+      mode="single"
+      selected={props.selected}
+      onSelect={props.setSelected}
+      footer={props.footer}
+      disabled={{ before: props.today }}
+    />
+  );
 }
