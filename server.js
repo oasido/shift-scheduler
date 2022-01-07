47d169759a23b8ca154b36806224c407d568f4c5
Ofek Asido <asido.ofek@gmail.com>
2022-01-07 23:55:45 +0200

    Add post endpoint for requested dates

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const app = express();
 const PORT = 4080;
 require('dotenv').config();
 
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 const mongoose = require('mongoose');
-const User = require('./models/user');
+const User = require('./models/User');
+const blockedDates = require('./models/blockedDates');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/shift-scheduler');
 
 passport.use(User.createStrategy());
@@ -17,6 +21,14 @@ app.get('/', async (req, res) => {
   await user.setPassword('password');
   await user.save();
   res.send('done');
+});
+
+app.post('/request', async (req, res) => {
+  try {
+    const requestDate = req.body;
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.listen(PORT, console.log(`Server is running on http://localhost:${PORT}`));
