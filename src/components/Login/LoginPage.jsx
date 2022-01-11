a10afc481ee9205141444bea3260bb87cb36e207
Ofek Asido <asido.ofek@gmail.com>
2022-01-11 22:59:16 +0200

    Set up login page

--- src/components/Login/LoginPage.jsx
+++ src/components/Login/LoginPage.jsx
@@ -1,23 +1,57 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    try {
+      e.preventDefault();
+      const response = await axios.post('/login', { username, password });
+      console.log(response);
+    } catch (error) {
+      // console.error(error.message);
+    }
+  };
+
   return (
     <>
       <section className="flex justify-center items-center min-h-screen bg-gray-800">
-        <div className="max-w-md w-full bg-gray-900 rounded p-6 space-y-4">
-          <div className="mb-4" dir="rtl">
-            <p className="text-gray-400">מרחבא!</p>
-            <h2 className="text-xl font-bold text-white">התחבר לפוקסיט</h2>
-          </div>
-          <div>
-            <input className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600" type="text" placeholder="Email" />
-          </div>
-          <div>
-            <input className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600" type="text" placeholder="Password" />
-          </div>
-          <div>
-            <button className="w-full py-4 bg-blue-600 hover:bg-blue-700 rounded text-sm font-bold text-gray-50 transition duration-200">התחבר</button>
-          </div>
+        <div className="max-w-md w-full bg-gray-900 rounded p-6">
+          <form onSubmit={handleLogin} className="space-y-4">
+            <div className="mb-4" dir="rtl">
+              <p className="text-gray-400">מרחבא!</p>
+              <h2 className="text-xl font-bold text-white">התחבר לפוקסיט</h2>
+            </div>
+            <div>
+              <input
+                className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
+                type="text"
+                placeholder="שם משתמש"
+                name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div>
+              <input
+                className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
+                type="password"
+                placeholder="סיסמא"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div>
+              <button className="w-full py-4 bg-blue-600 hover:bg-blue-700 rounded text-sm font-bold text-gray-50 transition duration-200" type="submit">
+                התחבר
+              </button>
+            </div>
 
-          <div className="flex justify-end text-white">הרשם</div>
+            <div className="flex justify-end text-white">הרשם</div>
+          </form>
         </div>
       </section>
     </>
