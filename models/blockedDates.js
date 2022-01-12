db956d4944902f8cd4ca74186157ef4e896a8e5d
Ofek Asido <asido.ofek@gmail.com>
2022-01-12 22:29:29 +0200

    Remove blockedDates schema

--- models/blockedDates.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const blockedDates = new Schema({
-  userID: { type: String, required: true },
-  dates: { type: String, required: true },
-});
-
-module.exports = mongoose.model('blockedDates', blockedDates);
