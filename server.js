c2685cd65ee0a2d3674795324365d5964c788c88
Ofek Asido <asido.ofek@gmail.com>
2022-01-15 19:22:43 +0200

    Generate hash and salt from given password

--- server.js
+++ server.js
@@ -164,4 +164,14 @@ const validPassword = (password, hash, salt) => {
   return hash === hashVerify;
 };
 
+const genPassword = (password) => {
+  var salt = crypto.randomBytes(32).toString('hex');
+  var genHash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+
+  return {
+    salt: salt,
+    hash: genHash,
+  };
+};
+
 app.listen(PORT, console.log(`Server is running on http://localhost:${PORT}`));
