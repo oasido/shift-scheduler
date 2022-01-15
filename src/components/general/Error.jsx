f0a3c0fdbf5093796c108d1f09d2d276a04d27b4
Ofek Asido <asido.ofek@gmail.com>
2022-01-15 23:49:53 +0200

    Change class to className

--- src/components/general/Error.jsx
+++ src/components/general/Error.jsx
@@ -1,9 +1,9 @@
 const Error = ({ bolded, msg }) => {
   return (
     <>
-      <div dir="rtl" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-        <strong class="font-bold">{bolded}</strong>
-        <span class="block sm:inline mr-3">{msg}</span>
+      <div dir="rtl" className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+        <strong className="font-bold">{bolded}</strong>
+        <span className="block sm:inline mr-3">{msg}</span>
       </div>
     </>
   );
