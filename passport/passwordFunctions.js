aac6d63768f6fbc5da1cbff25eb2993d1d58c941
Ofek Asido <asido.ofek@gmail.com>
2022-01-19 22:12:34 +0200

    Separate functions related to passwords

---
+++ passport/passwordFunctions.js
@@ -0,0 +1,19 @@
+const crypto = require('crypto');
+
+const validPassword = (password, hash, salt) => {
+  var hashVerify = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+  return hash === hashVerify;
+};
+
+const genPassword = (password) => {
+  var salt = crypto.randomBytes(32).toString('hex');
+  var genHash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+
+  return {
+    salt: salt,
+    hash: genHash,
+  };
+};
+
+module.exports = validPassword;
+module.exports = genPassword;
