54986c0c39f44cccd61fc0ba9bb388c1fd1b84d1
Ofek Asido <asido.ofek@gmail.com>
2022-02-09 19:14:45 +0200

    Split RequestsList to components

--- src/components/AdminControlPanel/RequestsList.jsx
+++ src/components/AdminControlPanel/RequestsList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import RequestsListTableRow from './RequestsListTableRow';
 
-export default function RequestsList() {
+export default function RequestsList({ requests }) {
   return (
     <>
       <div className="mx-auto w-5/6 mt-5">
@@ -14,45 +15,8 @@ export default function RequestsList() {
           <div className="px-6 pt-6">
             <table className="w-full whitespace-nowrap">
               <tbody>
-                <tr>
-                  <td>
-                    <div className="flex items-center">
-                      <div className="pl-3">
-                        <div className="flex items-center text-sm leading-none">
-                          <p className="font-semibold text-gray-800">פלוני אלמוני</p>
-                        </div>
-                        <p className="text-xs md:text-sm leading-none text-gray-600 mt-2">10.10.1111</p>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="w-1/3 lg:w-1/6">
-                    <div>
-                      <div className="flex items-center justify-center px-2 py-2 mt-2 bg-green-200 rounded-full">
-                        <p className="text-xs leading-3 text-green-700">מאושר</p>
-                      </div>
-                    </div>
-                  </td>
-                </tr>
-
-                <tr>
-                  <td className="pt-6">
-                    <div className="flex items-center">
-                      <div className="pl-3">
-                        <div className="flex items-center text-sm leading-none">
-                          <p className="font-semibold text-gray-800">חוטם חוטמן</p>
-                        </div>
-                        <p className="text-xs md:text-sm leading-none text-gray-600 mt-2">10.10.1111</p>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="w-1/3 lg:w-1/6">
-                    <div>
-                      <div className="flex items-center justify-center px-2 py-2 mt-2 bg-yellow-200 rounded-full">
-                        <p className="text-xs leading-3 text-yellow-700">בהמתנה</p>
-                      </div>
-                    </div>
-                  </td>
-                </tr>
+                <RequestsListTableRow name={'בוטי בוט'} date={'1/1/1111'} status={true} />
+                <RequestsListTableRow name={'ניק ניקוס'} date={'1/1/1111'} status={true} />
               </tbody>
             </table>
           </div>
