6800b714dcf8a210048a60a0fa53e36f7927fdc3
Ofek Asido <asido.ofek@gmail.com>
2022-02-25 19:55:50 +0200

    Lower the margin on the Msg component

--- src/components/general/Msg.jsx
+++ src/components/general/Msg.jsx
@@ -5,8 +5,8 @@ const Msg = ({ bolded, msg, OK }) => {
         dir="rtl"
         className={
           OK
-            ? `bg-green-100 border border-green-400 text-green-700 mt-5 mb-12 px-4 py-3 rounded relative`
-            : `bg-red-100 border border-red-400 text-red-700 mt-5 mb-12 px-4 py-3 rounded relative`
+            ? `bg-green-100 border border-green-400 text-green-700 my-3 px-4 py-3 rounded relative`
+            : `bg-red-100 border border-red-400 text-red-700 my-3 px-4 py-3 rounded relative`
         }
       >
         <strong className="font-bold">{bolded}</strong>
