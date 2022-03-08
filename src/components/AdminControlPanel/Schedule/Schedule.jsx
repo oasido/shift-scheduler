00d4fae8cd656535ddcf0c1f43b175b01a50f8d5
Ofek Asido <asido.ofek@gmail.com>
2022-03-08 23:40:15 +0200

    (TODO) Add shift posting capability

--- src/components/AdminControlPanel/Schedule/Schedule.jsx
+++ src/components/AdminControlPanel/Schedule/Schedule.jsx
@@ -73,6 +73,7 @@ const Schedule = () => {
 
   const uploadSchedule = (e) => {
     e.preventDefault();
+    // add here a post request to the server w/ the json schedule object to be sent to the database
   };
 
   const formatDay = (date) => {
