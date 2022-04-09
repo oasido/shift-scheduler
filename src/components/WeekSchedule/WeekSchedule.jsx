68923bd2952e500f28881fb592079d2bea206b36
Ofek Asido <asido.ofek@gmail.com>
2022-04-09 19:17:06 +0300

    Remove redundant commented div

--- src/components/WeekSchedule/WeekSchedule.jsx
+++ src/components/WeekSchedule/WeekSchedule.jsx
@@ -144,7 +144,6 @@ const WeekSchedule = () => {
                   datesArr={datesArr}
                   formatDay={formatDay}
                 />
-                {/* </div> */}
               </div>
             </div>
           </div>
