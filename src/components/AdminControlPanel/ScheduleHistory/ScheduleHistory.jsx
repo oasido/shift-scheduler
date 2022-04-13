af483b9736aab1435a100c49530ecce14a6f4b16
Ofek Asido <asido.ofek@gmail.com>
2022-04-13 18:47:49 +0300

    Add schedule history with ability to remove schedules

--- src/components/AdminControlPanel/ScheduleHistory/ScheduleHistory.jsx
+++ src/components/AdminControlPanel/ScheduleHistory/ScheduleHistory.jsx
@@ -5,6 +5,9 @@ import { useUserContext } from '../../useUserContext';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { format } from 'date-fns';
+import { FcCancel, FcCalendar, FcManager, FcAlarmClock } from 'react-icons/fc';
+import { RiHashtag } from 'react-icons/ri';
+import Swal from 'sweetalert2';
 
 const ScheduleHistory = () => {
   const { user } = useUserContext();
@@ -12,8 +15,13 @@ const ScheduleHistory = () => {
 
   let navigate = useNavigate();
 
+  const refreshShifts = async () => {
+    const response = await axios.get('/getScheduleHistory');
+    setShifts(response.data);
+  };
+
   useEffect(() => {
-    axios.get('/getScheduleHistory').then((response) => setShifts(response.data));
+    refreshShifts();
   }, []);
 
   switch (true) {
@@ -36,6 +44,28 @@ const ScheduleHistory = () => {
       break;
   }
 
+  const handleRemove = async (e, id) => {
+    e.preventDefault();
+
+    Swal.fire({
+      title: '?האם להמשיך',
+      text: 'לא ניתן לשחזר משמרת שנמחקה',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'ביטול',
+      confirmButtonText: 'כן, מחק משמרת',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios.post('/removeSchedule', { id });
+        refreshShifts();
+        // Swal.fire('!משמרת נמחקה', 'המשמרת נמחקה בהצלחה, רענן את העמוד לראות שינויים', 'success');
+        Swal.fire('!משמרת נמחקה', 'המשמרת נמחקה בהצלחה', 'success');
+      }
+    });
+  };
+
   return (
     <>
       <Navbar />
@@ -43,33 +73,70 @@ const ScheduleHistory = () => {
         <div className="grid mt-5 place-items-center" dir="rtl">
           <div className="w-11/12 lg:w-4/6">
             <h1 className="text-3xl font-semibold">סידורי עבודה</h1>
-            <table className="w-full mx-auto my-10 border-separate shadow-sm table-auto md:w-5/6 lg:w-4/6">
-              <thead>
-                <tr className="text-xl text-right">
-                  <th className="w-1/12">#</th>
-                  <th className="w-5/12">תאריך</th>
-                  <th className="w-2/12">שעה</th>
-                  <th className="w-3/12">ע"י</th>
-                  <th className="w-1/12"></th>
-                </tr>
-              </thead>
-              <tbody>
-                {shifts &&
-                  shifts.map((shift, i) => {
-                    const date = new Date(Date.parse(shift.date));
-                    const time = format(date, 'HH:mm');
-                    return (
-                      <tr className="text-lg font-medium text-gray-900" key={shift._id}>
-                        <td>{i + 1}</td>
-                        <td>{shift.name}</td>
-                        <td>{time}</td>
-                        <td>{shift.savedBy}</td>
-                        <td>X</td>
-                      </tr>
-                    );
-                  })}
-              </tbody>
-            </table>
+            {shifts.length > 0 && (
+              <table className="w-full mx-auto my-10 border-separate shadow-sm table-auto md:w-5/6 lg:w-4/6">
+                <thead>
+                  <tr className="text-xl text-right">
+                    <th className="w-1/12">
+                      <RiHashtag />
+                    </th>
+                    <th className="w-5/12">
+                      <div className="flex">
+                        <FcCalendar className="mx-1 mt-1" />
+                        <p className="">תאריך</p>
+                      </div>
+                    </th>
+                    <th className="w-2/12">
+                      <div className="flex">
+                        <FcAlarmClock className="mx-1 mt-1" />
+                        שעה
+                      </div>
+                    </th>
+                    <th className="w-3/12">
+                      <div className="flex">
+                        <FcManager className="mx-1 mt-1" />
+                        ע"י
+                      </div>
+                    </th>
+                    <th className="w-1/12"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {shifts &&
+                    shifts
+                      .slice(0)
+                      .reverse()
+                      .map((shift, i) => {
+                        const date = new Date(Date.parse(shift.date));
+                        const time = format(date, 'HH:mm');
+                        return (
+                          <tr
+                            className="text-lg font-medium text-gray-900 hover:bg-slate-100"
+                            key={shift._id}
+                          >
+                            <td className="text-slate-700">{shifts.length - i}</td>
+                            <td>{shift.name}</td>
+                            <td>{time}</td>
+                            <td>{shift.savedBy}</td>
+                            <td>
+                              <button className="p-2 " onClick={(e) => handleRemove(e, shift._id)}>
+                                <FcCancel className="text-2xl" />
+                              </button>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                </tbody>
+              </table>
+            )}
+            {shifts.length === 0 && (
+              <>
+                <div className="text-2xl font-medium text-center my-28">
+                  <h1>לא נמצאו משמרות</h1>
+                  <h1>יש ליצור סידור עבודה חדש</h1>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
