0401148a2c5446ad6baa04cec5e7cd9cb5c6d689
Ofek Asido <asido.ofek@gmail.com>
2022-04-14 00:11:51 +0300

    Show relevant requests and add ability to show all history

--- src/components/User/Requests/RequestsList.jsx
+++ src/components/User/Requests/RequestsList.jsx
@@ -1,37 +1,88 @@
 import { useUserContext } from '../../useUserContext';
 import RequestsListTableRow from './RequestsListTableRow';
+import { parse, isAfter } from 'date-fns';
+import { useState } from 'react';
+import { Button, Collapse } from '@mantine/core';
 
 export default function RequestsList() {
   const { user } = useUserContext();
+  const [opened, setOpen] = useState(false);
+
+  const blockedDates = [...user.blockedDates];
+
+  blockedDates.forEach((date, i) => {
+    const parsedDate = parse(date.date, 'dd-MM-yyyy', new Date());
+
+    if (isAfter(parsedDate, new Date())) {
+      console.log(date.date);
+    } else {
+      delete blockedDates[i];
+    }
+  });
 
   return (
     <>
       <div className="mx-auto mt-5 md:w-10/12 lg:w-11/12">
-        <div className="border rounded-lg pb-6 border-gray-200">
-          <div className="flex items-center border-b border-gray-200 justify-between px-6 py-3">
-            <p className="text-lg lg:text-xl font-semibold leading-tight text-gray-800">
+        <div className="pb-6 border border-gray-200 rounded-lg">
+          <div className="flex items-center justify-between px-6 py-3 border-b border-gray-200">
+            <p className="text-lg font-semibold leading-tight text-gray-800 lg:text-xl">
               הבקשות שלי
             </p>
             <div className="flex cursor-pointer items-center justify-center px-3 ml-1 py-2.5 border rounded border-gray-100">
-              <p className="text-xs md:text-sm leading-none text-gray-600">סנן לפי: חדש</p>
+              {/* <p className="text-xs leading-none text-gray-600 md:text-sm">סנן לפי: חדש</p> */}
             </div>
           </div>
           <div className="px-4 pt-4">
             <table className="w-full">
               <tbody>
-                {user.blockedDates.map((date) => {
-                  return (
-                    <RequestsListTableRow
-                      key={date._id}
-                      comment={date.comment}
-                      date={date.date}
-                      status={date.approved}
-                      dateID={date._id}
-                    />
-                  );
-                })}
+                {blockedDates
+                  .slice(0)
+                  .reverse()
+                  .map((date) => {
+                    return (
+                      <RequestsListTableRow
+                        key={date._id}
+                        comment={date.comment}
+                        date={date.date}
+                        status={date.approved}
+                        dateID={date._id}
+                      />
+                    );
+                  })}
               </tbody>
             </table>
+            {blockedDates.length > 0 ? (
+              <div className="flex justify-center mt-5">
+                <Button className="text-lg bg-gray-600" onClick={() => setOpen((o) => !o)}>
+                  ראה הכל
+                </Button>
+              </div>
+            ) : (
+              <h1 className="text-2xl font-medium text-center my-28 text-slate-800">
+                אין בקשות לחסימת תאריכים
+              </h1>
+            )}
+            <Collapse in={opened}>
+              {/* old  */}
+              <table className="w-full mt-2">
+                <tbody>
+                  {user.blockedDates
+                    .slice(0)
+                    .reverse()
+                    .map((date) => {
+                      return (
+                        <RequestsListTableRow
+                          key={date._id}
+                          comment={date.comment}
+                          date={date.date}
+                          status={date.approved}
+                          dateID={date._id}
+                        />
+                      );
+                    })}
+                </tbody>
+              </table>
+            </Collapse>
           </div>
         </div>
       </div>
