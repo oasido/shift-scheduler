0ff56d66b2ca4cef10a165280542dbb094fa2766
Ofek Asido <asido.ofek@gmail.com>
2022-04-22 19:52:20 +0300

    Fix button working only when you press the text on small screens

--- src/components/AdminControlPanel/Navbar.jsx
+++ src/components/AdminControlPanel/Navbar.jsx
@@ -165,19 +165,21 @@ const Navbar = () => {
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="span"
-                  className={classNames(
-                    item.current
-                      ? 'bg-gray-900 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block px-3 py-2 rounded-md text-lg font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  <Link to={item.href}>{item.name}</Link>
-                </Disclosure.Button>
+                <Link to={item.href} key={item.name}>
+                  <Disclosure.Button
+                    key={item.name}
+                    as="span"
+                    className={classNames(
+                      item.current
+                        ? 'bg-gray-900 text-white'
+                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'block px-3 py-2 rounded-md text-lg font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                </Link>
               ))}
             </div>
           </Disclosure.Panel>
